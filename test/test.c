1e20e1a8b38bcd4c658fee93db4a03b3b44fb7dd
asabon <asabon.t@gmail.com>
2021-02-20 22:48:42 +0900

    Update test.c

--- test/test.c
+++ test/test.c
@@ -7,7 +7,16 @@ int main(void)
     STATEMACHINE_T statemachine;
     STATE_T state;
     result = statemachine_init(&statemachine);
+    if (result != 0) {
+        return -1;
+    }
     result = statemachine_add_state(&statemachine, &state);
+    if (result != 0) {
+        return -1;
+    }
     result = statemachine_do(&statemachine);
+    if (result != 0) {
+        return -1;
+    }
     return 0;
 }
