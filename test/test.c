829ad90564f79ecb7c7221220e6149f1647ad148
asabon <asabon.t@gmail.com>
2021-02-20 23:35:52 +0900

    Update test.c

--- test/test.c
+++ test/test.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include "statemachine.h"
 
+int init_entry(void)
+{
+    return 0;
+}
+
+int init_do(void)
+{
+    return 0;
+}
+
+int init_exit(void)
+{
+    return 0;
+}
+
 int main(void)
 {
     int result;
     STATEMACHINE_T statemachine;
-    STATE_T state;
-    result = statemachine_init(&statemachine);
-    if (result != 0) {
-        return -1;
-    }
-    result = statemachine_add_state(&statemachine, &state);
+    STATE_T statelist[] = {
+        {0, init_entry, init_do, init_exit},
+        {1, init_entry, init_do, init_exit}
+    };
+    result = statemachine_init(&statemachine, &statelist);
     if (result != 0) {
         return -1;
     }
