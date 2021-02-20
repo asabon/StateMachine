f49b51b2713ea8eb4329cf61045c212c18bf4074
asabon <asabon.t@gmail.com>
2021-02-20 23:43:05 +0900

    Update test.c

--- test/test.c
+++ test/test.c
@@ -16,15 +16,46 @@ int init_exit(void)
     return 0;
 }
 
+int sleep_entry(void)
+{
+    return 0;
+}
+
+int sleep_do(void)
+{
+    return 0;
+}
+
+int sleep_exit(void)
+{
+    return 0;
+}
+
+int active_entry(void)
+{
+    return 0;
+}
+
+int active_do(void)
+{
+    return 0;
+}
+
+int active_exit(void)
+{
+    return 0;
+}
+
 int main(void)
 {
     int result;
     STATEMACHINE_T statemachine;
     STATE_T statelist[] = {
-        {0, init_entry, init_do, init_exit},
-        {1, init_entry, init_do, init_exit}
+        {0, init_entry,   init_do,   init_exit},
+        {1, sleep_entry,  sleep_do,  sleep_exit},
+        {2, active_entry, active_do, active_exit}
     };
-    result = statemachine_init(&statemachine, &statelist);
+    result = statemachine_init(&statemachine, &&statelist);
     if (result != 0) {
         return -1;
     }
