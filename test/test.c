c96f14eb6032cbcbbc5e4f7062948b1eb3df5d7a
asabon <asabon.t@gmail.com>
2021-02-21 23:38:17 +0900

    Update test.c

--- test/test.c
+++ test/test.c
@@ -29,6 +29,39 @@ void callcnt_init(void)
     }
 }
 
+int callcnt_check(int ientry, int ido, int iexit, int sentry, int sdo, int sexit, int aentry, int ado, int aexit)
+{
+    int error_cnt = 0;
+    if (callcnt[INIT_ENTRY] != ientry){
+        error_cnt++;
+    }
+    if (callcnt[INIT_DO] != ido){
+        error_cnt++;
+    }
+    if (callcnt[INIT_EXIT] != iexit){
+        error_cnt++;
+    }
+    if (callcnt[SLEEP_ENTRY] != sentry){
+        error_cnt++;
+    }
+    if (callcnt[SLEEP_DO] != sdo){
+        error_cnt++;
+    }
+    if (callcnt[SLEEP_EXIT] != sexit){
+        error_cnt++;
+    }
+    if (callcnt[ACTIVE_ENTRY] != aentry){
+        error_cnt++;
+    }
+    if (callcnt[ACTIVE_DO] != ado){
+        error_cnt++;
+    }
+    if (callcnt[ACTIVE_EXIT] != aexit){
+        error_cnt++;
+    }
+    return error_cnt;
+}
+
 int init_entry(void)
 {
     callcnt[INIT_ENTRY]++;
@@ -96,27 +129,36 @@ int main(void)
         {1, sleep_entry,  sleep_do,  sleep_exit},
         {2, active_entry, active_do, active_exit}
     };
+
     result = statemachine_init(&statemachine, statelist, sizeof(statelist)/sizeof(statelist[0]), 0);
     if (result != 0) {
         errorcnt++;
     }
-    if (callcnt[INIT_ENTRY] != 1) {
+    if (callcnt_check(1, 0, 0, 0, 0, 0, 0, 0, 0) != 0) {
         errorcnt++;
     }
+
     result = statemachine_do(&statemachine);
     if (result != 0) {
         errorcnt++;
     }
+    if (callcnt_check(1, 1, 1, 1, 0, 0, 0, 0, 0) != 0) {
+        errorcnt++;
+    }
+
     result = statemachine_do(&statemachine);
     if (result != 0) {
         errorcnt++;
     }
+    if (callcnt_check(1, 1, 1, 1, 1, 1, 1, 0, 0) != 0) {
+        errorcnt++;
+    }
+
     result = statemachine_do(&statemachine);
     if (result != 0) {
         errorcnt++;
     }
-    result = statemachine_do(&statemachine);
-    if (result != 0) {
+    if (callcnt_check(1, 1, 1, 2, 1, 1, 1, 1, 1) != 0) {
         errorcnt++;
     }
     return errorcnt;
