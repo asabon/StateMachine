936f970322ff03f768d4541914e88c39e28eda64
asabon <asabon.t@gmail.com>
2021-02-21 23:03:12 +0900

    Update statemachine.c

--- src/statemachine.c
+++ src/statemachine.c
@@ -4,7 +4,7 @@ int statemachine_init(STATEMACHINE_T * pStatemachine, STATE_T * pState, int stat
 {
     pStatemachine->pState = pState;
     pStatemachine->currentState = initialState;
-    pStatemachine->statelen;
+    pStatemachine->statelen = statelen;
     result = pStatemachine->pState[initialState]->pf_Entry();
     if (result != 0) {
         return result;
