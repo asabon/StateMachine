cb062b9f37bf669eabd2eb5e0f523b2e810ac753
asabon <asabon.t@gmail.com>
2021-02-21 22:42:01 +0900

    add statelen

--- src/statemachine.c
+++ src/statemachine.c
@@ -1,10 +1,10 @@
 #include "statemachine.h"
 
-int statemachine_init(STATEMACHINE_T * pStatemachine, STATE_T * pState, int initialState)
+int statemachine_init(STATEMACHINE_T * pStatemachine, STATE_T * pState, int statelen, int initialState)
 {
     pStatemachine->pState = pState;
     pStatemachine->currentState = initialState;
-    printf ("len = %ld\n", sizeof(pState)/sizeof(STATE_T));
+    pStatemachine->statelen;
     return 0;
 }
 
--- src/statemachine.h
+++ src/statemachine.h
@@ -16,10 +16,11 @@ typedef struct {
 typedef struct {
     int currentState;
     int nextState;
+    int statelen;
     STATE_T * pState;
 } STATEMACHINE_T;
 
-extern int statemachine_init(STATEMACHINE_T * pStatemachine, STATE_T * pState, int initialState);
+extern int statemachine_init(STATEMACHINE_T * pStatemachine, STATE_T * pState, int statelen, int initialState);
 extern int statemachine_do(STATEMACHINE_T * pStatemachine);
 
 #endif /* STATEMACHINE_H */
