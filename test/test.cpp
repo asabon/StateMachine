ab4680395c3d080935f6222ee74f6a25887012b7
asabon <asabon.t@gmail.com>
2021-02-26 00:00:27 +0900

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -2,130 +2,6 @@
 #include <gmock/gmock.h>
 #include "statemachine.h"
 
-#if 0
-enum {
-    INIT = 0,
-    SLEEP,
-    ACTIVE
-};
-
-enum {
-    INIT_ENTRY = 0,
-    INIT_DO,
-    INIT_EXIT,
-    SLEEP_ENTRY,
-    SLEEP_DO,
-    SLEEP_EXIT,
-    ACTIVE_ENTRY,
-    ACTIVE_DO,
-    ACTIVE_EXIT
-};
-
-int callcnt[9];
-
-void callcnt_init(void)
-{
-    int i;
-    for(i = 0; i < sizeof(callcnt)/sizeof(callcnt[0]); i++) {
-        callcnt[i] = 0;
-    }
-}
-
-int callcnt_check(int ientry, int ido, int iexit, int sentry, int sdo, int sexit, int aentry, int ado, int aexit)
-{
-    int error_cnt = 0;
-    if (callcnt[INIT_ENTRY] != ientry){
-        error_cnt++;
-    }
-    if (callcnt[INIT_DO] != ido){
-        error_cnt++;
-    }
-    if (callcnt[INIT_EXIT] != iexit){
-        error_cnt++;
-    }
-    if (callcnt[SLEEP_ENTRY] != sentry){
-        error_cnt++;
-    }
-    if (callcnt[SLEEP_DO] != sdo){
-        error_cnt++;
-    }
-    if (callcnt[SLEEP_EXIT] != sexit){
-        error_cnt++;
-    }
-    if (callcnt[ACTIVE_ENTRY] != aentry){
-        error_cnt++;
-    }
-    if (callcnt[ACTIVE_DO] != ado){
-        error_cnt++;
-    }
-    if (callcnt[ACTIVE_EXIT] != aexit){
-        error_cnt++;
-    }
-    if (error_cnt != 0) {
-        printf ("%d %d %d %d %d %d %d %d %d\n", ientry, ido, iexit, sentry, sdo, sexit, aentry, ado, aexit);
-        printf ("%d %d %d %d %d %d %d %d %d\n", callcnt[INIT_ENTRY], callcnt[INIT_DO], callcnt[INIT_EXIT], callcnt[SLEEP_ENTRY], callcnt[SLEEP_DO], callcnt[SLEEP_EXIT], callcnt[ACTIVE_ENTRY], callcnt[ACTIVE_DO], callcnt[ACTIVE_EXIT]);
-    }
-    return error_cnt;
-}
-
-int init_entry(void)
-{
-    callcnt[INIT_ENTRY]++;
-    return 0;
-}
-
-int init_do(int * pNextState)
-{
-    callcnt[INIT_DO]++;
-    *pNextState = SLEEP;
-    return 0;
-}
-
-int init_exit(void)
-{
-    callcnt[INIT_EXIT]++;
-    return 0;
-}
-
-int sleep_entry(void)
-{
-    callcnt[SLEEP_ENTRY]++;
-    return 0;
-}
-
-int sleep_do(int * pNextState)
-{
-    callcnt[SLEEP_DO]++;
-    *pNextState = ACTIVE;
-    return 0;
-}
-
-int sleep_exit(void)
-{
-    callcnt[SLEEP_EXIT]++;
-    return 0;
-}
-
-int active_entry(void)
-{
-    callcnt[ACTIVE_ENTRY]++;
-    return 0;
-}
-
-int active_do(int * pNextState)
-{
-    callcnt[ACTIVE_DO]++;
-    *pNextState = SLEEP;
-    return 0;
-}
-
-int active_exit(void)
-{
-    callcnt[ACTIVE_EXIT]++;
-    return 0;
-}
-#endif
-
 TEST(statemachine_init, test_00)
 {
     int result;
@@ -215,6 +91,9 @@ extern "C" {
 int state10_entry(void);
 int state10_do(int * pNext);
 int state10_exit(void);
+int state11_entry(void);
+int state11_do(int * pNext);
+int state11_exit(void);
 }
 
 int state10_entry(void) {
@@ -227,6 +106,18 @@ int state10_do(int * pNext) {
 
 int state10_exit(void) {
     return mocks->state10_exit();
+}
+
+int state11_entry(void) {
+    return mocks->state11_entry();
+}
+
+int state11_do(int * pNext) {
+    return mocks->state11_do(pNext);
+}
+
+int state11_exit(void) {
+    return mocks->state11_exit();
 }
 
 class Test_statemachine_do : public ::testing::Test {
@@ -244,8 +135,8 @@ TEST_F(Test_statemachine_do, test_01)
     int result;
     STATEMACHINE_T statemachine;
     STATE_T statelist[] = {
-        {state00_entry, state00_do, state00_exit},
-        {state01_entry, state01_do, state01_exit}
+        {state10_entry, state10_do, state10_exit},
+        {state11_entry, state11_do, state11_exit}
     };
 
     result = statemachine_init(&statemachine, statelist, sizeof(statelist)/sizeof(statelist[0]), 0);
@@ -253,58 +144,3 @@ TEST_F(Test_statemachine_do, test_01)
     result = statemachine_do(&statemachine);
     EXPECT_EQ(0, result);
 }
-
-#if 0
-int main(void)
-{
-    int result;
-    int errorcnt = 0;
-    STATEMACHINE_T statemachine;
-    STATE_T statelist[] = {
-        {init_entry,   init_do,   init_exit},
-        {sleep_entry,  sleep_do,  sleep_exit},
-        {active_entry, active_do, active_exit}
-    };
-
-    result = statemachine_init(&statemachine, statelist, sizeof(statelist)/sizeof(statelist[0]), 0);
-    if (result != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-    if (callcnt_check(0, 0, 0, 0, 0, 0, 0, 0, 0) != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-
-    result = statemachine_do(&statemachine);
-    if (result != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-    if (callcnt_check(1, 1, 1, 0, 0, 0, 0, 0, 0) != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-
-    result = statemachine_do(&statemachine);
-    if (result != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-    if (callcnt_check(1, 1, 1, 1, 1, 1, 0, 0, 0) != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-
-    result = statemachine_do(&statemachine);
-    if (result != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-    if (callcnt_check(1, 1, 1, 1, 1, 1, 1, 1, 1) != 0) {
-        printf ("%d\n", __LINE__);
-        errorcnt++;
-    }
-    return errorcnt;
-}
-#endif
