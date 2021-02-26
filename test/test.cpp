f5672060dc122da92282b2eba66e8c17423365ae
asabon <asabon.t@gmail.com>
2021-02-26 22:19:50 +0900

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -220,3 +220,47 @@ TEST_F(Test_statemachine_do, test_04)
     result = statemachine_do(&statemachine);
     EXPECT_EQ(-1, result);
 }
+
+TEST_F(Test_statemachine_do, test_05)
+{
+    int result;
+    STATEMACHINE_T statemachine;
+    STATE_T statelist[] = {
+        {state10_entry, state10_do, state10_exit},
+        {state11_entry, state11_do, state11_exit}
+    };
+
+    result = statemachine_init(&statemachine, statelist, sizeof(statelist)/sizeof(statelist[0]), 0);
+    EXPECT_EQ(0, result);
+
+    EXPECT_CALL(*mocks, state10_entry()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(*mocks, state10_do(testing::_)).WillRepeatedly(testing::DoAll(testing::SetArgPointee<0>(1), testing::Return(-2)));
+    EXPECT_CALL(*mocks, state10_exit()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(*mocks, state11_entry()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(*mocks, state11_do(testing::_)).WillRepeatedly(testing::DoAll(testing::SetArgPointee<0>(0), testing::Return(0)));
+    EXPECT_CALL(*mocks, state11_exit()).WillRepeatedly(testing::Return(0));
+    result = statemachine_do(&statemachine);
+    EXPECT_EQ(-2, result);
+}
+
+TEST_F(Test_statemachine_do, test_06)
+{
+    int result;
+    STATEMACHINE_T statemachine;
+    STATE_T statelist[] = {
+        {state10_entry, state10_do, state10_exit},
+        {state11_entry, state11_do, state11_exit}
+    };
+
+    result = statemachine_init(&statemachine, statelist, sizeof(statelist)/sizeof(statelist[0]), 0);
+    EXPECT_EQ(0, result);
+
+    EXPECT_CALL(*mocks, state10_entry()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(*mocks, state10_do(testing::_)).WillRepeatedly(testing::DoAll(testing::SetArgPointee<0>(1), testing::Return(0)));
+    EXPECT_CALL(*mocks, state10_exit()).WillRepeatedly(testing::Return(-3));
+    EXPECT_CALL(*mocks, state11_entry()).WillRepeatedly(testing::Return(0));
+    EXPECT_CALL(*mocks, state11_do(testing::_)).WillRepeatedly(testing::DoAll(testing::SetArgPointee<0>(0), testing::Return(0)));
+    EXPECT_CALL(*mocks, state11_exit()).WillRepeatedly(testing::Return(0));
+    result = statemachine_do(&statemachine);
+    EXPECT_EQ(-3, result);
+}
