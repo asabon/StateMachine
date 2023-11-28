77ccc49a53c095ecde66a8640d6ae3aae9c5d48c
Takafumi Asahara <asabon.t@gmail.com>
2023-11-29 02:42:24 +0900

    modify test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,64 @@
-//#include "gmock/gmock.h"
 #include "gtest/gtest.h"
-//#include "mocks.h"
-#include "../src/statemachine.h"
+#include "statemachine.h"
 
-TEST(statemachine_init, test_00)
+/* ===== Mock codes ===== */
+
+void init_mock(void)
+{
+    state_00_entry_count = 0;
+    state_00_entry_result = 0;
+    state_00_do_count = 0;
+    state_00_do_next = 0;
+    state_00_do_result = 0;
+    state_00_exit_count = 0;
+    state_00_exit_result = 0;
+}
+
+static int state_00_entry_count = 0;
+static int state_00_entry_result = 0;
+int state_00_entry(void)
+{
+    state_00_entry_count++;
+    return state_00_entry_result;
+}
+
+static int state_00_do_count = 0;
+static int state_00_do_next = 0;
+static int state_00_do_result = 0;
+int state_00_do(int * pNextState)
+{
+    state_00_do_count++;
+    *pNextState = state_00_do_next;
+    return state_00_do_result;
+}
+
+static int state_00_exit_count = 0;
+static int state_00_exit_result = 0;
+int state_00_exit(void)
+{
+    state_00_exit_count++;
+    return state_00_exit_result;
+}
+
+/* ===== Test codes ===== */
+
+/*
+ * Test target    : statemachine_init()
+ * Test condition : statelist pointer is NULL.
+ */
+TEST(statemachine_init, test_statelist_is_null)
+{
+    int result;
+    STATEMACHINE_T statemachine;
+    result = statemachine_init(&statemachine, NULL, 0, 0);
+    EXPECT_EQ(ERROR_INIT_NULL, result);
+}
+
+/*
+ * Test target    : statemachine_init()
+ * Test condition : statelist has one item but all null functions.
+ */
+TEST(statemachine_init, test_statelist_has_one_item)
 {
     int result;
     STATE_T statelist[] = {
@@ -13,15 +68,11 @@ TEST(statemachine_init, test_00)
     EXPECT_EQ(ERROR_INIT_NULL, result);
 }
 
-TEST(statemachine_init, test_01)
-{
-    int result;
-    STATEMACHINE_T statemachine;
-    result = statemachine_init(&statemachine, NULL, 0, 0);
-    EXPECT_EQ(ERROR_INIT_NULL, result);
-}
-
-TEST(statemachine_init, test_02)
+/*
+ * Test target    : statemachine_init()
+ * Test condition : statelist has one item but all null functions and length is 0.
+ */
+TEST(statemachine_init, test_statelist_has_one_item_but_length_is_0)
 {
     int result;
     STATEMACHINE_T statemachine;
@@ -32,7 +83,11 @@ TEST(statemachine_init, test_02)
     EXPECT_EQ(ERROR_INIT_NULL, result);
 }
 
-TEST(statemachine_init, test_03)
+/*
+ * Test target    : statemachine_init()
+ * Test condition : statelist has one item but all null functions and length is 1.
+ */
+TEST(statemachine_init, test_statelist_has_one_item_and_length_is_1)
 {
     int result;
     STATEMACHINE_T statemachine;
@@ -45,6 +100,32 @@ TEST(statemachine_init, test_03)
     EXPECT_EQ(1, statemachine.statelen);
     EXPECT_EQ(0, statemachine.currentState);
     EXPECT_EQ(1, statemachine.changed);
+}
+
+TEST(statemachine_do, test_do_01)
+{
+    int result;
+    STATEMACHINE_T statemachine;
+    STATE_T statelist[] = {
+        {state_00_entry, state_00_do, state_00_exit},
+    };
+    /* Initialize mock */
+    init_mock();
+    /* Initialize statemachine */
+    result = statemachine_init(&statemachine, statelist, sizeof(statelist)/sizeof(statelist[0]), 0);
+    EXPECT_EQ(0, result);
+    EXPECT_EQ(0, state_00_entry_count);
+    EXPECT_EQ(0, state_00_do_count);
+    EXPECT_EQ(0, state_00_exit_count);
+    /* Do statemachine */
+    state_00_entry_result = 0;
+    state_00_do_result = 0;
+    state_00_exit_result = 0;
+    result = statemachine_do(&statemachine);
+    EXPECT_EQ(0, result);
+    EXPECT_EQ(1, state_00_entry_count);
+    EXPECT_EQ(1, state_00_do_count);
+    EXPECT_EQ(1, state_00_exit_count);
 }
 
 #if 0
@@ -190,3 +271,23 @@ TEST_F(Test_statemachine_do, test_06)
     EXPECT_EQ(-3, result);
 }
 #endif
+
+static int state_00_entry_result = 0;
+int state_00_entry(void)
+{
+    return state_00_entry_result;
+}
+
+static int state_00_do_next = 0;
+static int state_00_do_result = 0;
+int state_00_do(int * pNextState)
+{
+    *pNextState = state_00_do_next;
+    return state_00_do_result;
+}
+
+static int state_00_exit_result = 0;
+int state_00_exit(void)
+{
+    return state_00_exit_result;
+}
